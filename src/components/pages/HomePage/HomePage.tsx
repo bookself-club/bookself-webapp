de02d48e1cd9b2f3a48d4c2760367035846f8478
Marcelo Dapper <marcelo.dapper@gmail.com>
2018-08-07 17:43:02 -0300

    Upgrade styled-components

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -5,6 +5,7 @@ import HomeTemplate from 'components/templates/HomeTemplate';
 const HomePage: React.SFC = () => (
   <HomeTemplate>
     <h1>Home Page</h1>
+    <a href="/login">Login page</a>
   </HomeTemplate>
 );
 
