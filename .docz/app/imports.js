93c5a15b437066ab4e198a6ed9bfc2c0eb8c68b6
Marcelo Dapper <marcelo.dapper@gmail.com>
2018-08-16 18:45:23 -0300

    Remove .docz folder and add it to gitignore

--- .docz/app/imports.js
+++
@@ -1,4 +0,0 @@
-export const imports = {
-  'src/components/atoms/Button/README.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-atoms-button-readme" */ 'src/components/atoms/Button/README.mdx'),
-};
